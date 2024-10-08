1d42e70ae16038de2e5c25b729a14e3c7174edbb
Johannes Lindvall <johannes.lindvall@xolaris.se>
2024-10-08 15:03:58 +0200

    Adding bootstrap ui framework and intial startpage component.

---
+++ src/main/webui/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {StartPageComponent} from "./start/start-page.component";
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    AppComponent,
+    StartPageComponent
+  ]
+})
---
+++ src/main/webui/src/app/start/start-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'start-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './start-page.component.html',
+})
+
+export class StartPageComponent {
+
+}
+
