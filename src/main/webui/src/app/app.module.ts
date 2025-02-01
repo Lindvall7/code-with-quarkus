1b124b649aae71f5b3bdd259146a58ad31ce71a5
johannes <jlindvall94@gmail.com>
2025-02-01 20:10:58 +0100

    updating todo and adding word service

--- src/main/webui/src/app/app.module.ts
+++ src/main/webui/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {StartPageComponent} from "./start/start-page.component";
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
+    StartPageComponent
 
   ],
   imports: [
     BrowserModule,
     AppComponent,
     StartPageComponent
+    HttpClientModule
   ]
 })
